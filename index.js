1d35d23b68c063fb69b502d3875f2674a17711d7
RaLLy08 <uytemataaero@mail.ru>
2022-08-14 11:24:39 +0400

    fix

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const ByteSwitcher = require('./ByteCompare');
+const ByteCompare = require('./ByteCompare');
 
 const myState = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-const bs = new ByteSwitcher(myState);
+const bs = new ByteCompare(myState);
 
 const a = bs.createState(['a', 'b']);
 const b = bs.createState(['a', 'b']);
