b9a65c8d41db120b83272cdb6bc17e444318274c
RaLLy08 <uytemataaero@mail.ru>
2022-08-14 11:23:54 +0400

    fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const ByteSwitcher = require('./ByteSwitcher');
+const ByteSwitcher = require('./ByteCompare');
 
 const myState = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
@@ -6,12 +6,12 @@ const bs = new ByteSwitcher(myState);
 
 const a = bs.createState(['a', 'b']);
 const b = bs.createState(['a', 'b']);
-const c = bs.createState([]);
+const c = bs.createState(['c', 'b']);
 
 
 console.log(b.has(a));
 // get includes ? has
-// excudes
+// exclude
 // same
 // bench
 // html
